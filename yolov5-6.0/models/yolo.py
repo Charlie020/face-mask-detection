d4987f6164691b9d37abf49cb7d9da8351708d8c
Louis LYC <2983736120@qq.com>
2023-04-26 19:57:56 +0800

    ODconv

--- yolov5-6.0/models/yolo.py
+++ yolov5-6.0/models/yolo.py
@@ -263,7 +263,7 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
 
         n = n_ = max(round(n * gd), 1) if n > 1 else n  # depth gain
         if m in [Conv, GhostConv, Bottleneck, GhostBottleneck, SPP, SPPF, DWConv, MixConv2d, Focus, CrossConv,
-                 BottleneckCSP, C3, C3TR, C3SPP, C3Ghost, CBAM, CBAMC3, CoordAtt, SE,C3STR]:
+                 BottleneckCSP, C3, C3TR, C3SPP, C3Ghost, CBAM, CBAMC3, CoordAtt, SE,C3STR,ODConv]:
             c1, c2 = ch[f], args[0]
             if c2 != no:  # if not output
                 c2 = make_divisible(c2 * gw, 8)
@@ -306,6 +306,14 @@ def parse_model(d, ch):  # model_dict, input_channels(3)
             if m is C3HB:
                 args.insert(2, n)
                 n = 1
+        elif m in [ODConv]:
+            c1, c2 = ch[f], args[0]
+            if c2 != no:  # if not output
+                c2 = make_divisible(c2 * gw, 8)
+
+            args = [c1, c2, *args[1:]]
+
+
         else:
             c2 = ch[f]
 
