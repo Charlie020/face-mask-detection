a3877fe8fd0381aeafbdadd42d1ce2b75b2e2b24
25803 <2580390720@qq.com>
2023-05-06 23:48:58 +0800

    增加了BiFPN与小目标检测的结合

--- yolov5-6.0/train_scheduler.py
+++ yolov5-6.0/train_scheduler.py
@@ -1,9 +1,9 @@
 import os
 
 # 第一个命令
-command_1 = 'python train.py --data mask.yaml --cfg models/yolov5s_CA+SmallTarget.yaml --weights yolov5s.pt --batch-size 16 --epochs 200'
+command_1 = 'python train.py --data mask.yaml --cfg models/yolov5s_ODconv.yaml --weights yolov5s.pt --batch-size 16 --epochs 200'
 os.system(command_1)
 
-# # 第二个命令
-command_2 = 'python train.py --data mask.yaml --cfg models/yolov5s_CBAM+SmallTarget.yaml --weights yolov5s.pt --batch-size 16 --epochs 200'
-os.system(command_2)
+# # # 第二个命令
+# command_2 = 'python train.py --data mask.yaml --cfg models/yolov5s_gnconv.yaml --weights yolov5s.pt --batch-size 16 --epochs 200'
+# os.system(command_2)
