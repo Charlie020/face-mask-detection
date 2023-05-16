53a1ea737ca318a6b2ae398b35b720c68007c059
25803 <2580390720@qq.com>
2023-05-16 09:04:37 +0800

    修正了CA的网络结构，增加了小目标检测与CA的融合

--- yolov5-6.0/train_scheduler.py
+++ yolov5-6.0/train_scheduler.py
@@ -1,9 +1,9 @@
 import os
 
 # 第一个命令
-command_1 = 'python train.py --data mask.yaml --cfg models/yolov5s_CA.yaml --weights yolov5s.pt --batch-size 16 --epochs 200'
+command_1 = 'python train.py --data mask.yaml --cfg models/yolov5s_SmallTarget+CA.yaml --weights yolov5s.pt --batch-size 16 --epochs 200'
 os.system(command_1)
 
 # 第二个命令
-command_2 = 'python train.py --data mask.yaml --cfg models/yolov5s_SmallTarget+C3HB_backbone.yaml --weights yolov5s.pt --batch-size 16 --epochs 200'
+command_2 = 'python train.py --data mask.yaml --cfg models/yolov5s_CA.yaml --weights yolov5s.pt --batch-size 16 --epochs 200'
 os.system(command_2)
