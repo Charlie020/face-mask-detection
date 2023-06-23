a406e3bc3dd86123ff6a2801f6c54515e0f274ed
25803 <2580390720@qq.com>
2023-06-23 08:08:31 +0800

    增加了划分数据集的脚本make.py

--- yolov5-6.0/VOCdevkit/XMLLabel_TransTo_TXTLabel.py
+++ yolov5-6.0/VOCdevkit/XMLLabel_TransTo_TXTLabel.py
@@ -1,18 +1,15 @@
 import xml.etree.ElementTree as ET
-import pickle
 import os
-from os import listdir, getcwd
-from os.path import join
 
 sets = ['train', 'test', 'val']
+
 # 仅需修改以下路径
-Imgpath = 'E:\PythonCode\MaskDetect_YOLOv5-6.0\yolov5-6.0\VOCdevkit\JPEGImages'                   # VOC的JPEGImages文件夹路径
-xmlfilepath = 'E:\PythonCode\MaskDetect_YOLOv5-6.0\yolov5-6.0\VOCdevkit\Annotations'              # VOC的Annotations文件夹路径
-ImageSets_path = 'E:\PythonCode\MaskDetect_YOLOv5-6.0\yolov5-6.0\VOCdevkit\ImageSets\Main'        # VOC中ImagesSets的Main文件夹的路径
-Label_path = 'E:\PythonCode\MaskDetect_YOLOv5-6.0\yolov5-6.0\VOCdevkit'                           # 将XML格式的标签转化为TXT格式后保存的路径
-classes = ["nomask", "mask"]                                                                      # 类别名
-
-
+# 由于yolov5读的图片文件夹名为images，所以需要先将voc数据集中的JPEGImages重命名为images，再修改以下路径（注意路径不是反斜杠）
+Imgpath = 'E:/PythonCode/MaskDetect_YOLOv5-6.0/yolov5-6.0/VOCdevkit/images/'                      # voc的原JPEGImages文件夹
+xmlfilepath = 'E:/PythonCode/MaskDetect_YOLOv5-6.0/yolov5-6.0/VOCdevkit/Annotations/'             # voc的Annotations标签文件夹
+ImageSets_path = 'E:/PythonCode/MaskDetect_YOLOv5-6.0/yolov5-6.0/VOCdevkit/ImageSets/Main/'       # voc的ImageSets中Main文件夹
+Label_path = 'E:/PythonCode/MaskDetect_YOLOv5-6.0/yolov5-6.0/VOCdevkit/'                          # 类名
+classes = ["nomask", "mask"]
 
 def convert(size, box):
     dw = 1. / size[0]
@@ -29,8 +26,8 @@ def convert(size, box):
 
 
 def convert_annotation(image_id):
-    in_file = open(xmlfilepath + '\%s.xml' % (image_id), encoding='utf-8')
-    out_file = open(Label_path + '\labels/%s.txt' % (image_id), 'w')
+    in_file = open(xmlfilepath + '%s.xml' % (image_id), encoding="utf-8")
+    out_file = open(Label_path + 'labels/%s.txt' % (image_id), 'w')
     tree = ET.parse(in_file)
     root = tree.getroot()
     size = root.find('size')
@@ -54,11 +51,11 @@ def convert_annotation(image_id):
 
 
 for image_set in sets:
-    if not os.path.exists(Label_path + '\labels\\'):
-        os.makedirs(Label_path + '\labels\\')
-    image_ids = open(ImageSets_path + '\%s.txt' % (image_set)).read().strip().split()
-    list_file = open(Label_path + '\%s.txt' % (image_set), 'w')
+    if not os.path.exists(Label_path + 'labels/'):
+        os.makedirs(Label_path + 'labels/')
+    image_ids = open(ImageSets_path + '%s.txt' % (image_set)).read().strip().split()
+    list_file = open(Label_path + '%s.txt' % (image_set), 'w')
     for image_id in image_ids:
-        list_file.write(Imgpath + '\%s.jpg\n' % (image_id))
+        list_file.write(Imgpath + '%s.jpg\n' % (image_id))
         convert_annotation(image_id)
     list_file.close()
