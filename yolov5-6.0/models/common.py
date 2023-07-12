59f8791b3e907abd565a23fabb6f2249103a6527
25803 <2580390720@qq.com>
2023-07-12 09:10:56 +0800

    添加了SimAM

--- yolov5-6.0/models/common.py
+++ yolov5-6.0/models/common.py
@@ -2118,3 +2118,21 @@ class DenseBlock(nn.Module):
         # return torch.cat((self.layer(self.cv1(x)), self.cv2(x)), dim=1)
 #-------------------------Done------------------------------
 
+#------------------------SimAM-----------------------------
+class SimAM(torch.nn.Module):
+    def __init__(self, channels=None, out_channels=None, e_lambda=1e-4):
+        super(SimAM, self).__init__()
+
+        self.activaton = nn.Sigmoid()
+        self.e_lambda = e_lambda
+
+    def forward(self, x):
+        b, c, h, w = x.size()
+
+        n = w * h - 1
+
+        x_minus_mu_square = (x - x.mean(dim=[2, 3], keepdim=True)).pow(2)
+        y = x_minus_mu_square / (4 * (x_minus_mu_square.sum(dim=[2, 3], keepdim=True) / n + self.e_lambda)) + 0.5
+
+        return x * self.activaton(y)
+#------------------------DONE------------------------------
